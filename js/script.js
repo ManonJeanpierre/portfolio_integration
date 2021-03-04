1b12db47609908019b50064db6dd6d05125ca457
ManonJeanpierre <manon.jeanpierre@edu.univ-fcomte.fr>
2021-03-04 13:21:17 +0100

    Test

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ $(document).ready( function() {
     $('.filter-button-group').on( 'click', 'li', function() {
         var filterValue = $(this).attr('data-filter');
         $('.grid').isotope({ filter: filterValue });
-        $('.filter-button-group li').removeClass('active');
-        $(this).addClass('active');
+        $('.filter-button-group li').removeClass('activefilter');
+        $(this).addClass('activefilter');
     });
 })
 
